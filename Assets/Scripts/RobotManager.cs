f87b143d5417273e2e3fdfe4639934b352bdada6
StijnOnline <stijnvandeijzen@gmail.com>
2020-01-31 23:40:33 +0100

    Robotmanager, robot entry and exit

--- Assets/Scripts/RobotManager.cs
+++ Assets/Scripts/RobotManager.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class RobotManager : MonoBehaviour
 {
     public GameObject[] robotPrefabs;
+    public GameObject currentRobot;
     public Transform spawnPoint;
+    public HingeJoint futonHinge;
     public void SpawnRobot()
     {
-        Instantiate(robotPrefabs[Random.Range(0, robotPrefabs.Length)],spawnPoint.position,Quaternion.identity);
+        if (currentRobot != null)
+            return;
+        futonHinge.useSpring = false;
+        GameObject ob = Instantiate(robotPrefabs[Random.Range(0, robotPrefabs.Length)],spawnPoint.position,Quaternion.identity);
+        currentRobot = ob;
+        currentRobot.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll & ~  RigidbodyConstraints.FreezePositionY;
     }
 
     public void RobotDone()
     {
+        futonHinge.useSpring = true;
+        currentRobot.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         Debug.Log("Done");
+    }
+}
+
+[CustomEditor(typeof(RobotManager))]
+public class RobotManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        RobotManager myTarget = (RobotManager)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Spawn"))
+        {
+            myTarget.SpawnRobot();
+        }
+
+        if (GUILayout.Button("Done"))
+        {
+            myTarget.RobotDone();
+        }
     }
 }
