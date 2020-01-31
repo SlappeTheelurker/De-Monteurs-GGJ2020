6fd57b05ec699725fab322a0be752ebe8ff8403f
MidasMilan <midasmilos@gmail.com>
2020-01-31 21:05:12 +0100

    Add basic Grijper movement (no easing); Add ps4 button detection;

---
+++ Assets/Scripts/ButtonInputTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInputTest : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Square"))
+        {
+            Debug.Log("Vierkantje!");
+        }
+
+        if (Input.GetButtonDown("X"))
+        {
+            Debug.Log("Iksje!");
+        }
+
+        if (Input.GetButtonDown("Circle"))
+        {
+            Debug.Log("Rondje!");
+        }
+
+        if (Input.GetButtonDown("Triangle"))
+        {
+            Debug.Log("Driehoekje!");
+        }
+    }
+}
---
+++ Assets/Scripts/GrijperMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrijperMovement : MonoBehaviour
+{
+    private bool movementEnabled = true;
+    [SerializeField] private float maxMoveSpeed = 1.0f;
+    private Rigidbody thisRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (thisRB == null) thisRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movementEnabled)
+        {
+            Vector3 movementInput = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            movementInput.Normalize();
+            movementInput *= maxMoveSpeed;
+            thisRB.MovePosition(thisRB.position + movementInput);
+        }
+    }
+}
