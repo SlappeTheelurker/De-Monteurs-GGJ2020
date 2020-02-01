7b43dbc408c89f82c6fe9506626f772334d3f4ef
MidasMilan <midasmilos@gmail.com>
2020-02-01 22:14:56 +0100

    Fix grab bugs;

--- Assets/Robot.cs
+++ Assets/Robot.cs
@@ -97,18 +97,4 @@ public class Robot : MonoBehaviour
 
         return Error;
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.GetComponent<Chip>() != null)
-        {
-            if (other.GetComponent<Chip>().claw != null)
-            {
-                other.GetComponent<Chip>().claw.Ungrab();
-            }
-
-            other.transform.position = OutOfBoundsSpawnpoint.position;
-            //other.transform.rotation = OutOfBoundsSpawnpoint.rotation;
-        }
-    }
 }
