4414ff29880acd7ad039af9657ead2439e0901e8
Wessel Poldervaart <wessel@jip-creates.nl>
2020-02-02 09:05:14 +0100

    SoundFX Click chip and ambient music + Chip materials
    jottem!

--- Assets/Scripts/ChipSocket.cs
+++ Assets/Scripts/ChipSocket.cs
@@ -7,29 +7,48 @@ public class ChipSocket : MonoBehaviour
     public Chip socketedChip;
     public Transform snapPoint;
     public int chipOrientation; // -1 = none
-    
+  
     private float tolerance = 30f;
 
-    [Header("Correct data")]
+	[Header("Correct data")]
     public bool needChip;
     public Chip.ChipFormats format;
     public string type;
     public int correctOrientation; // 0-3 w/ 0=360
-    
-    
-    private void OnTriggerEnter(Collider other)
+	
+    #region audioshit
+    private AudioSource audioSource;
+
+	private AudioClip clickClip;
+	
+
+    #endregion 
+
+    private void Start(){
+        audioSource = gameObject.AddComponent<AudioSource>();;
+    }
+
+	private void OnTriggerEnter(Collider other)
     {
         Chip chip = other.GetComponent<Chip>();
         if (chip != null && socketedChip == null)
         {
-            if (chip.claw != null)
+
+			if (chip.claw != null)
             {
                 if (!chip.claw.getUngrabAllowed()) { return; }
             }
 
             chip.claw?.Ungrab();
+			
+            //sfx
+            clickClip = Resources.Load("click1") as AudioClip;
+            Debug.Log(clickClip);
+			audioSource.clip = clickClip;
+			audioSource.Play();
 
-            float rot = other.transform.rotation.eulerAngles.y;
+
+			float rot = other.transform.rotation.eulerAngles.y;
             bool snap = false;
             if (Mathf.Abs(rot % 90) <= tolerance)
             {
